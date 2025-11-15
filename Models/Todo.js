bce08b274266f0c2d36fc258e05f10f6487243c9
AnjaliNahar <naharanjali26@gmail.com>
2025-11-15 18:05:20 +0530

    completedAt updated

--- Models/Todo.js
+++ Models/Todo.js
@@ -13,7 +13,24 @@ const todoSchema = new mongoose.Schema({
    },
     dueDate: {
     type: Date, // ⏰ Time till the task should be completed
-  }
+  },
+  completedAt: {
+      type: Date,
+      default: null,
+    },
+
+
+
+  // ⭐ TTL Index: deletes doc 24 hours after completedAt
+  expireAt: {
+    type: Date,
+    default: function () {
+      return this.completedAt;
+    },
+    index: {
+      expireAfterSeconds: 86400, // 24 hours
+    },
+  },
 
 },{timestamps:true})
 
